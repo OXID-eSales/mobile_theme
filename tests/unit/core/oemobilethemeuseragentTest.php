93ddf9e0860bc4917a378cf2832673f37a43cc80
l3k <linas.kukulskis@gmail.com>
2013-06-13 16:16:51 +0300

    test fix

--- tests/unit/core/oemobilethemeuseragentTest.php
+++ tests/unit/core/oemobilethemeuseragentTest.php
@@ -11,7 +11,7 @@
 require_once realpath( "." ).'/unit/OxidTestCase.php';
 require_once realpath( "." ).'/unit/test_config.inc.php';
 
-$sModuleDir = realpath( "." ) . '/../source/modules/oe/oemobiletheme/';
+$sModuleDir = realpath( "../" ) . '/source/modules/oe/oemobiletheme/';
 
 require_once $sModuleDir . 'core/oemobilethemeuseragent.php';
 
@@ -28,7 +28,7 @@ class Unit_Core_oeMobileThemeUserAgentTest extends OxidTestCase
             array( 'Mozilla/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3A100a Safari/419.3', 'mobile' ),
             array( 'Mozilla/5.0 (webOS/1.0; U; en-US) AppleWebKit/525.27.1 (KHTML, like Gecko) Version/1.0 Safari/525.27.1 Pre/1.0', 'mobile' ),
             array( 'Mozilla/4.0 (compatible; MSIE 8.0; AOL 9.1; AOLBuild 4334.34; Windows NT 5.1; SV1; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 1.1.4322)', 'desktop' ),
-            array( 'Mozilla/5.0 (Linux; U; Android 0.5; en-us) AppleWebKit/522+ (KHTML, like Gecko) Safari/419.3', 'desktop' ),
+            array( 'Mozilla/5.0 (Linux; U; Android 0.5; en-us) AppleWebKit/522+ (KHTML, like Gecko) Safari/419.3', 'mobile' ),
             array( 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0', 'desktop' )
         );
     }
