c50026f2c7c6abc223c206cef398536653f309a0
l3k <linas.kukulskis@gmail.com>
2013-06-14 14:04:55 +0300

    mobile theme module translations

---
+++ source/modules/oe/oemobiletheme/views/admin/en/module_options.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * #PHPHEADER_OXID_LICENSE_INFORMATION#
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   lang
+ * @copyright (c) OXID eSales AG 2003-#OXID_VERSION_YEAR#
+ * @version   SVN: $Id: theme_lang.php 28697 2010-06-29 11:09:58Z vilma $
+ */
+
+$aLang = array(
+'charset'                                       => 'ISO-8859-15',
+
+'SHOP_MODULE_GROUP_main'                       => 'Main',
+'SHOP_MODULE_sMobileTheme'                     => 'Mobile theme name',
+
+);
