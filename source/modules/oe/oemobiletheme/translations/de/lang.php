7de3ae7873e3d92d5a24cc0a99bd02757a933a8b
l3k <linas.kukulskis@gmail.com>
2013-06-14 13:35:49 +0300

    added module translations, which extends azure theme languages

---
+++ source/modules/oe/oemobiletheme/translations/de/lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * #PHPHEADER_OXID_LICENSE_INFORMATION#
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   lang
+ * @copyright (c) OXID eSales AG 2003-#OXID_VERSION_YEAR#
+ * @version   SVN: $Id: lang.php 28697 2010-06-29 11:09:58Z vilma $
+ */
+
+
+/* -----------------------
+--  ADAPTING THIS FILE  --
+--------------------------
+If you want to adapt this language file, use cust_lang.php instead.
+Further information is available in the manual at
+http://www.oxid-esales.com/en/resources/help-faq/eshop-manual/editing-language-files
+*/
+
+$sLangName  = "Deutsch";
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+
+    'charset'                                                     => "ISO-8859-15",
+    'MOBILE_DISPLAY'                                              => "Zu Mobile wechseln",
+);
