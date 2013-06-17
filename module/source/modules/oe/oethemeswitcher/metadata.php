1dca6308df05a29275ee5a224c11d569bf6a3b66
l3k <linas.kukulskis@gmail.com>
2013-06-17 14:08:03 +0300

    change module name

--- module/source/modules/oe/oethemeswitcher/metadata.php
+++ module/source/modules/oe/oethemeswitcher/metadata.php
@@ -26,22 +26,22 @@ $aModule = array(
     'url'          => 'http://www.oxid-esales.com',
     'email'        => 'info@oxid-esales.com',
     'extend'       => array(
-        'oxconfig'         => 'oe/oemobiletheme/core/oemobilethemeconfig',
-        'oxtheme'          => 'oe/oemobiletheme/core/oemobilethemetheme',
-        'oxviewconfig'     => 'oe/oemobiletheme/core/oemobilethemeviewconfig',
-        'manufacturerlist' => 'oe/oemobiletheme/controllers/oemobilethememanufacturerlist',
-        'alist'            => 'oe/oemobiletheme/controllers/oemobilethemealist',
-        'content'          => 'oe/oemobiletheme/controllers/oemobilethemecontent',
-        'details'          => 'oe/oemobiletheme/controllers/oemobilethemedetails',
-        'review'           => 'oe/oemobiletheme/controllers/oemobilethemereview',
-        'rss'              => 'oe/oemobiletheme/controllers/oemobilethemerss',
-        'start'            => 'oe/oemobiletheme/controllers/oemobilethemestart',
-        'tag'              => 'oe/oemobiletheme/controllers/oemobilethemetag',
-        'vendorlist'       => 'oe/oemobiletheme/controllers/oemobilethemevendorlist',
-        'oxlang'           => 'oe/oemobiletheme/core/oemobilethemelang',
+        'oxconfig'         => 'oe/oethemeswitcher/core/oemobilethemeconfig',
+        'oxtheme'          => 'oe/oethemeswitcher/core/oemobilethemetheme',
+        'oxviewconfig'     => 'oe/oethemeswitcher/core/oemobilethemeviewconfig',
+        'manufacturerlist' => 'oe/oethemeswitcher/controllers/oemobilethememanufacturerlist',
+        'alist'            => 'oe/oethemeswitcher/controllers/oemobilethemealist',
+        'content'          => 'oe/oethemeswitcher/controllers/oemobilethemecontent',
+        'details'          => 'oe/oethemeswitcher/controllers/oemobilethemedetails',
+        'review'           => 'oe/oethemeswitcher/controllers/oemobilethemereview',
+        'rss'              => 'oe/oethemeswitcher/controllers/oemobilethemerss',
+        'start'            => 'oe/oethemeswitcher/controllers/oemobilethemestart',
+        'tag'              => 'oe/oethemeswitcher/controllers/oemobilethemetag',
+        'vendorlist'       => 'oe/oethemeswitcher/controllers/oemobilethemevendorlist',
+        'oxlang'           => 'oe/oethemeswitcher/core/oemobilethemelang',
     ),
     'files' => array(
-        'oemobilethemeuseragent' => 'oe/oemobiletheme/core/oemobilethemeuseragent.php'
+        'oethemeswitcheruseragent' => 'oe/oethemeswitcher/core/oethemeswitcheruseragent.php'
     ),
 
     'blocks' => array(
