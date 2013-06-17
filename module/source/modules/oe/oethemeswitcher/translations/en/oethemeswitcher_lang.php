bcbb031c93f6503f34091fde5030b2b01a977388
l3k <linas.kukulskis@gmail.com>
2013-06-17 15:46:06 +0300

    added headers

--- module/source/modules/oe/oethemeswitcher/translations/en/oethemeswitcher_lang.php
+++ module/source/modules/oe/oethemeswitcher/translations/en/oethemeswitcher_lang.php
@@ -1,12 +1,24 @@
 <?php
 /**
- * #PHPHEADER_OXID_LICENSE_INFORMATION#
+ * This file is part of OXID eSales theme switcher module.
+ *
+ * OXID eSales theme switcher module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales theme switcher module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales theme switcher module.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @package   lang
- * @copyright (c) OXID eSales AG 2003-#OXID_VERSION_YEAR#
- * @version   SVN: $Id: lang.php 28697 2010-06-29 11:09:58Z vilma $
+ * @copyright (C) OXID eSales AG 2003-2013
  */
+
 
 
 /* -----------------------
