6bd28c9886ce3c8e67653aeec449780074dcf974
Artūras Ševčenko <arturas.sevcenko@gmail.com>
2013-06-27 14:17:32 +0300

    add paypal module lang constants (en/de)

--- module/source/modules/oe/oethemeswitcher/translations/de/oethemeswitcher_lang.php
+++ module/source/modules/oe/oethemeswitcher/translations/de/oethemeswitcher_lang.php
@@ -35,7 +35,25 @@ $sLangName  = "Deutsch";
 // RESOURCE IDENTIFIER = STRING
 // -------------------------------
 $aLang = array(
-
-    'charset'                                                     => "ISO-8859-15",
-    'MOBILE_DISPLAY'                                              => "Zu Mobile wechseln",
+    'charset'                                    => "ISO-8859-15",
+    'MOBILE_DISPLAY'                             => "Zu Mobile wechseln",
+    'OEPAYPAL_LOCALE'                            => 'de_DE',
+    'OEPAYPAL_PARTNERBOX_LINK'                   => 'https://www.paypal.com/de/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside',
+    'OEPAYPAL_ERROR_USER_ADDRESS'                => 'Ihre im Shop hinterlegten Adressinformationen stimmen nicht mit Ihrer PayPal-Lieferadresse überein. Bitte loggen Sie sich im Shop ein und führen Sie den PayPal Express Checkout noch einmal durch.',
+    'OEPAYPAL_BASKET_XPRESSBTN_IMG'              => 'https://www.paypal.com/de_DE/i/btn/btn_xpressCheckout.gif',
+    'OEPAYPAL_BASKET_XPRESSBTN_IMG_MOBILE'       => 'https://www.paypalobjects.com/webstatic/de_DE/i/de-btn-expresscheckoutmobile-195x37.png',
+    'OEPAYPAL_CHECKOUT_IMG_MOBILE'               => 'checkout-paypal-medium-de.png',
+    'OEPAYPAL_LOGO_IMG'                          => 'logo-paypal-medium-de.png',
+    'OEPAYPAL_PAYMENT_NOSHIPPINGFOUND'           => "Keine Versandarten gefunden. Bitte kontaktieren Sie uns telefonisch oder per E-Mail.",
+    'OEPAYPAL_PAYMENT_HELP_LINK'                 => 'https://www.paypal.com/de/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside',
+    'OEPAYPAL_PAYMENT_HELP_LINK_TEXT'            => 'Was ist PayPal?',
+    'OEPAYPAL_ORDER_SUBJECT'                     => 'Ihre Bestellung bei %s in Höhe von %s %s',
+    'OEPAYPAL_ORDER_CONF_SUBJECT'                => 'Bestellnummer %s',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL'          => 'Warenkorb in PayPal anzeigen',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL_HELPTEXT' => 'Der aktuelle Warenkorb wird auf der Zahlungsseite von PayPal angezeigt.',
+    'OEPAYPAL_PRICE'					         => 'Preis',
+    'OEPAYPAL_REQUIRED_FIELDS_EMPTY'	         => 'Bitte alle erforderlichen Felder ausfüllen.',
+    'OEPAYPAL_ORDER_ERROR'                       => 'Fehler im Bestellvorgang. Bitte versuchen Sie es später erneut oder wählen Sie eine andere Zahlungsart.',
+    'OEPAYPAL_RESPONSE_FROM_PAYPAL'              => 'Fehlermeldung von PayPal: ',
+    'OEPAYPAL_ORDER_TOTAL_HAS_CHANGED'           => 'Auf Grund Ihrer Auswahl im PayPal Express Checkout hat sich die Bestellsumme geändert. Bitte prüfen Sie Ihren Warenkorb und fahren Sie fort. Hinweis: Um den Express Checkout fortzusetzen, drücken Sie den Express Checkout Button erneut.',
 );
--- module/source/modules/oe/oethemeswitcher/translations/en/oethemeswitcher_lang.php
+++ module/source/modules/oe/oethemeswitcher/translations/en/oethemeswitcher_lang.php
@@ -35,7 +35,24 @@ $sLangName  = "English";
 // RESOURCE IDENTIFIER = STRING
 // -------------------------------
 $aLang = array(
-
-    'charset'                                                     => "ISO-8859-15",
-    'MOBILE_DISPLAY'                                              => "Switch to mobile",
+    'charset'                                    => "ISO-8859-15",
+    'MOBILE_DISPLAY'                             => "Switch to mobile",
+    'OEPAYPAL_LOCALE'                            => 'en_US',
+    'OEPAYPAL_PARTNERBOX_LINK'                   => 'https://www.paypal.com/en/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside',
+    'OEPAYPAL_ERROR_USER_ADDRESS'                => 'The address data provided in your shop account does not match your PayPal shipping address. Please log on to your shop account and start the PayPal Express Checkout process again.',
+    'OEPAYPAL_BASKET_XPRESSBTN_IMG'              => 'https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif',
+    'OEPAYPAL_CHECKOUT_IMG_MOBILE'               => 'checkout-paypal-medium-en.png',
+    'OEPAYPAL_LOGO_IMG'                          => 'logo-paypal-medium.png',
+    'OEPAYPAL_PAYMENT_HELP_LINK'                 => 'https://www.paypal.com/en/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside',
+    'OEPAYPAL_PAYMENT_HELP_LINK_TEXT'            => 'What is PayPal?',
+    'OEPAYPAL_PAYMENT_NOSHIPPINGFOUND'           => 'No shipping methods found. Please contact us by phone or email.',
+    'OEPAYPAL_ORDER_SUBJECT'                     => 'Your order at %s in the amount of %s %s',
+    'OEPAYPAL_ORDER_CONF_SUBJECT'                => 'Order number %s',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL'          => 'Display cart in PayPal',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL_HELPTEXT' => 'Display current shopping cart in payment page of PayPal.',
+    'OEPAYPAL_PRICE'					         => 'Price',
+    'OEPAYPAL_REQUIRED_FIELDS_EMPTY'	         => 'Please fill in all required fields.',
+    'OEPAYPAL_ORDER_ERROR'                       => 'Order execution error. Please try later or choose different payment method.',
+    'OEPAYPAL_RESPONSE_FROM_PAYPAL'              => 'Error message from PayPal: ',
+    'OEPAYPAL_ORDER_TOTAL_HAS_CHANGED'           => 'Based on your choice in PayPal Express Checkout, order total has changed. Please check your shopping cart and continue. Hint: for continuing with Express Checkout press Express Checkout button again.',
 );
