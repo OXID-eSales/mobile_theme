2299e56d86563a862ed0c5218a289e63151c54a9
l3k <linas.kukulskis@gmail.com>
2013-09-23 18:25:20 +0300

    changed version

--- theme/source/application/views/mobile/theme.php
+++ theme/source/application/views/mobile/theme.php
@@ -27,9 +27,6 @@ $aTheme = array(
     'title'        => 'OXID eShop mobile theme',
     'description'  => 'Your OXID eShop on mobile devices',
     'thumbnail'    => 'theme.jpg',
-    'version'      => '1.0',
+    'version'      => '1.1.0',
     'author'       => 'OXID'
-    //Define the following variables for the custom child themes:
-    //'parentTheme'    => 'azure',
-    //'parentVersions' => array('0.5','0.6'),
 );
