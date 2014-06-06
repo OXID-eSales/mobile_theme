90ed08cc12c07a0337f4afab0db4958d4a310d25
shiftas <Shiftass@gmail.com>
2014-06-06 14:09:42 +0300

    ESDEV-1817 Update mobile theme according to trusted shop changes.

--- theme/source/application/views/mobile/theme.php
+++ theme/source/application/views/mobile/theme.php
@@ -27,6 +27,6 @@ $aTheme = array(
     'title'        => 'OXID eShop mobile theme',
     'description'  => 'Your OXID eShop on mobile devices',
     'thumbnail'    => 'theme.jpg',
-    'version'      => '1.1.0',
+    'version'      => '1.1.1',
     'author'       => 'OXID'
 );
