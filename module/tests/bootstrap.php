8f0029fd03e8c340c57fa88d310bee9358e59f48
l3k <linas.kukulskis@gmail.com>
2014-09-17 13:43:44 +0300

    updated bootstrap

--- module/tests/bootstrap.php
+++ module/tests/bootstrap.php
@@ -11,44 +11,43 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
  * You should have received a copy of the GNU General Public License
  * along with OXID eSales theme switcher module.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2013
+ * @link          http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
  */
 
 if (getenv('oxPATH')) {
-    define ('oxPATH', getenv('oxPATH'));
+    define('oxPATH', getenv('oxPATH'));
     switch (getenv('OXID_VERSION')) {
         case 'EE':
-            define ('oxCCTempDir', '/tmp/oxCCTempDir_ee/');
-            define('OXID_VERSION_EE', true );
+            define('oxCCTempDir', '/tmp/oxCCTempDir_ee/');
+            define('OXID_VERSION_EE', true);
             define('OXID_VERSION_PE', false);
-            define('OXID_VERSION_PE_PE', false );
-            define('OXID_VERSION_PE_CE', false );
+            define('OXID_VERSION_PE_PE', false);
+            define('OXID_VERSION_PE_CE', false);
             define('OXID_VERSION_SUFIX', "_ee");
             break;
         case 'PE':
-            define ('oxCCTempDir', '/tmp/oxCCTempDir_pe/');
+            define('oxCCTempDir', '/tmp/oxCCTempDir_pe/');
             define('OXID_VERSION_EE', false);
-            define('OXID_VERSION_PE', true );
-            define('OXID_VERSION_PE_PE', true );
-            define('OXID_VERSION_PE_CE', false );
+            define('OXID_VERSION_PE', true);
+            define('OXID_VERSION_PE_PE', true);
+            define('OXID_VERSION_PE_CE', false);
             define('OXID_VERSION_SUFIX', "_pe");
             break;
         case 'CE':
-            define ('oxCCTempDir', '/tmp/oxCCTempDir_ce/');
+            define('oxCCTempDir', '/tmp/oxCCTempDir_ce/');
             define('OXID_VERSION_EE', false);
-            define('OXID_VERSION_PE', true );
-            define('OXID_VERSION_PE_PE', false );
-            define('OXID_VERSION_PE_CE', true );
+            define('OXID_VERSION_PE', true);
+            define('OXID_VERSION_PE_PE', false);
+            define('OXID_VERSION_PE_CE', true);
             define('OXID_VERSION_SUFIX', "_ce");
             break;
 
         default:
-            die('bad version : '."'".getenv('OXID_VERSION')."'");
+            die('bad version : ' . "'" . getenv('OXID_VERSION') . "'");
             break;
     }
 
@@ -56,23 +55,26 @@ if (getenv('oxPATH')) {
         mkdir(oxCCTempDir, 0777, 1);
     }
 } else {
-    define('OXID_VERSION_EE',    0 );
-    define('OXID_VERSION_PE_PE', 0 );
-    define('OXID_VERSION_PE_CE', 1 );
+    define('OXID_VERSION_EE', 0);
+    define('OXID_VERSION_PE_PE', 0);
+    define('OXID_VERSION_PE_CE', 1);
 
     // AUTOMATICALLY GENERATED CONSTANTS DEPENDING ON PREVIOUS
-    define('OXID_VERSION_PE',    OXID_VERSION_PE_CE || OXID_VERSION_PE_PE );
-    define('OXID_VERSION_SUFIX', (OXID_VERSION_EE?'_ee':'').(OXID_VERSION_PE_PE?'_pe':'').(OXID_VERSION_PE_CE?'_ce':'') );
-    define ('oxPATH', '/var/www/eshop/eshop/source/');
-    define ('oxCCTempDir', '/var/www/eshop/eshop/source/tmp/');
+    define('OXID_VERSION_PE', OXID_VERSION_PE_CE || OXID_VERSION_PE_PE);
+    define('OXID_VERSION_SUFIX', (OXID_VERSION_EE ? '_ee' : '') . (OXID_VERSION_PE_PE ? '_pe' : '') . (OXID_VERSION_PE_CE ? '_ce' : ''));
+    define('oxPATH', '/var/www/eshop/eshop/source/');
+    define('oxCCTempDir', '/var/www/eshop/eshop/source/tmp/');
 }
 
 if (!defined('OXID_VERSION_SUFIX')) {
     define('OXID_VERSION_SUFIX', '');
 }
 
-
-if (getenv('CODECOVERAGE')) {
-    // PHPUnit_Util_Filter configuration
-    PHPUnit_Util_Filter::$addUncoveredFilesFromWhitelist = true;
+if (file_exists("unit/DbRestore.php")) {
+    include_once "unit/DbRestore.php";
+} else {
+    include_once "DbRestore.php";
 }
+
+$oDbRestore = new DbRestore();
+$oDbRestore->dumpDB();
