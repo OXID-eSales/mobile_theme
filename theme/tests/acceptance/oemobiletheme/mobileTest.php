2683719bbeb9c221134c3adc2543bdd4dad8fc20
shiftas <Shiftass@gmail.com>
2015-01-14 12:58:51 +0200

    ESDEV-2625 Change oxTestCase path
    oxTestCase was moved to different place in shops' test library, so it has to be changed here too.

--- theme/tests/acceptance/oemobiletheme/mobileTest.php
+++ theme/tests/acceptance/oemobiletheme/mobileTest.php
@@ -19,8 +19,7 @@
  * @copyright (C) OXID eSales AG 2003-2014
  */
 
-require_once realpath( "." ) . '/acceptance/library/oxTestCase.php';
-
+require_once __DIR__ . '/../oxTestCase.php';
 
 /**
  * Acceptance test cases
